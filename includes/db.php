312728fe3af3a244809953f7c48a2e35d193ce6e
Kelechi Grant <kcgrant100@gmail.com>
2026-06-15 00:46:21 +0100

    new fix

--- includes/db.php
+++ includes/db.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $username = "root";
 $password = "";
-$database = "rectem_Resturant_db";
+$database = "rectem_cafeteria_db";
 
 $conn = new mysqli($host, $username, $password, $database);
 
